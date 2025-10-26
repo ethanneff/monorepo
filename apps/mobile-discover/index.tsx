b131ac29f7ea3e5baf897a0cd0411ce80697280b
Ethan Neff <ethan.neff@eneff.com>
2025-10-26 11:23:57 -0700

    Enable react-native-screens freeze in mobile apps
    Added enableFreeze(true) to index.tsx of all mobile app entry points to improve performance and memory usage by freezing inactive screens.

--- apps/mobile-discover/index.tsx
+++ apps/mobile-discover/index.tsx
@@ -1,7 +1,9 @@
 import '@expo/metro-runtime';
 import { gestureHandler } from '@shared/components';
 import { registerRootComponent } from 'expo';
+import { enableFreeze } from 'react-native-screens';
 import { App } from './src/App';
 
+enableFreeze(true);
 gestureHandler();
 registerRootComponent(App);
